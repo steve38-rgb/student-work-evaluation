8b3958537f20ad829cca59d1af00b6ec23ae429e
steve38-rgb <steve38@st.tc.edu.tw>
2026-01-28 14:43:24 +0800

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,4 @@
+const CONFIG = {
+  // 請在此填入您的 Google Apps Script 部署網址 (Web App URL)
+  GAS_API_URL: "https://script.google.com/macros/s/AKfycbxjKUU-vIYpqx8HDiLF-yHpoXR3bwGOs5V11R8JsJbQOmaKadbhbs8bykC-vkv1dkY_/exec"
+};
